3c663ae4b3981acc4cffd836c716415259e44552
hector-silvar <hector.silvar@mediamonks.com>
2022-03-26 21:35:09 -0600

    Basic spring initializer template

---
+++ src/test/java/mx/hexsilvar/skeleton/SpringBootSkeletonApplicationTests.java
@@ -0,0 +1,12 @@
+package mx.hexsilvar.skeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSkeletonApplicationTests {
+
+  @Test void contextLoads() {
+  }
+
+}
